0e681e3b26a5b8fdbd811f60200fd74f32e71873
Tinossoft <tinossoft@gmail.com>
2022-03-02 16:42:05 +0530

    newly updated

--- lib/leave_details/leave_details.dart
+++ lib/leave_details/leave_details.dart
@@ -156,58 +156,165 @@ class _LeaveDetailsPageState extends State<LeaveDetailsPage> {
       //   title: Text("REGISTER"),
       //   centerTitle: true,
       // ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 250,
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: StreamBuilder<QuerySnapshot>(
-                  stream: user,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10,top: 10, bottom: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+
+                  height: MediaQuery.of(context).size.height * 0.95,
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: StreamBuilder<QuerySnapshot>(
+                      stream: user,
+                      builder: (
+                          BuildContext context,
+                          AsyncSnapshot<QuerySnapshot> snapshot,
+                          ) {
+                        if(snapshot.hasError){
+                          return Text("Something wrong");
+                        }
+                        if(snapshot.connectionState == ConnectionState.waiting) {
+                          return Text("Loading");
+                        }
+                        final data = snapshot.requireData;
+
+                        return ListView.builder(
+
+                          itemCount: data.size,
+                          itemBuilder: (context, index) {
+                            return data.docs[index]["email"] == widget.emailId ? Padding(
+                              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10, top: 10),
+                              child: Container(
+                                child: Column(
+
+                                  children: [
+//------------------------------------------------------------------------------------------------------
+
+                                    Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Container(
+                                        child: Center(
+                                          child: Text("Date:   ${data.docs[index]["date"]}",
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                        width: double.infinity,
+                                        height: 50,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(15),
+                                          border: Border.all(color: Colors.white, width: 2),
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+//---------------------------------------------------------------------------------------------------------
+                                    Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Container(
+                                        child: Center(
+                                          child: Text("Type Of Day:   ${data.docs[index]["day-type"]}",
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                        width: double.infinity,
+                                        height: 50,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(15),
+                                          border: Border.all(color: Colors.white, width: 2),
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+//----------------------------------------------------------------------------------------------------
+                                    Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Container(
+                                        child: Center(
+                                          child: Text("Type Of Leave:   ${data.docs[index]["leave-type"]}",
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                        width: double.infinity,
+                                        height: 50,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(15),
+                                          border: Border.all(color: Colors.white, width: 2),
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+//----------------------------------------------------------------------------------------
+
+                                    Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Container(
+                                        child: SingleChildScrollView(
+                                          child: Padding(
+                                            padding: EdgeInsets.all(10),
+                                            child: Text("Reason:   ${data.docs[index]["reason"]}",
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+
+                                        width: double.infinity,
+                                        height: 150,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(15),
+                                          border: Border.all(color: Colors.white, width: 2),
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+//----------------------------------------------------------------------------------------------------------
+                                  ],
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(15),
+                                  border: Border.all(color: Colors.white, width: 2),
+                                ),
+                                width: double.infinity,
+                                height: 400,
+
+                              ),
+                            ) : Container();
 
 
-                  builder: (
-                      BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot,
-                      ) {
-                    if(snapshot.hasError){
-                      return Text("Something wrong");
-                    }
-                    if(snapshot.connectionState == ConnectionState.waiting) {
-                      return Text("Loading");
-                    }
-                    final data = snapshot.requireData;
+                          },
 
-                    return ListView.builder(
-
-                      itemCount: data.size,
-                      itemBuilder: (context, index) {
-                        return data.docs[index]["email"] == widget.emailId ? Container(
-                          child: Text(" ${data.docs[index]["reason"]}",
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            border: Border.all(color: Colors.teal, width: 2),
-                          ),
-                          width: double.infinity,
-                          height: 50,
-
-                        ) : Container(
-                          width: 100,
-                          height: 200,
-                          color: Colors.teal,
                         );
+                      }
 
-                      },
+                  ),
+                ),
 
-                    );
-                  }
               ),
-            ),
 
-          ],
+
+            ],
+          ),
         ),
       ),
 
--- lib/task/task.dart
+++ lib/task/task.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskPage extends StatefulWidget {
@@ -8,8 +9,237 @@ class TaskPage extends StatefulWidget {
 }
 
 class _TaskPageState extends State<TaskPage> {
+
+
+
+  DateTime _dateTime = DateTime.now();
+  final DateTime? date = DateTime.now();
+  //
+  // void _showDatePicker(){
+  //   showDatePicker(context: context,
+  //     initialDate: DateTime.now(),
+  //     firstDate: DateTime(1980),
+  //     lastDate: DateTime(2050),
+  //   ).then((value){
+  //     setState(() {
+  //       _dateTime = value!;
+  //     });
+  //   });
+  // }
+  DateTimeRange dateRange = DateTimeRange(
+    start: DateTime(2022, 11, 5),
+    end: DateTime(2022, 12, 24),
+  );
+
+  Future pickDateRange() async {
+    DateTimeRange? newDateRange = await showDateRangePicker(
+        context: context,
+        initialDateRange: dateRange,
+        firstDate: DateTime(1900),
+        lastDate: DateTime(2100),
+    );
+    if (newDateRange == null) return;   // pressed 'X'
+    setState(() {
+      dateRange = newDateRange;  // press save button
+    });
+  }
+
+
+
+  var buttonText = "Start Date";
+  var buttonText1 = "End Date";
+
+
+
+
+  final nameEditingController = TextEditingController();
+  final departmentNameEditingController = TextEditingController();
+
+  final item1 = ["FLUTTER","NODE", "REACT", "PYTHON"];
+  final item2 = ["Medical", "Casual"];
+  String? valueChoose1;
+  String? valueChoose2;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    final start = dateRange.start;
+    final end = dateRange.end;
+
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text(
+      //     "Task"
+      //   ),
+      // ),
+      // body: SingleChildScrollView(
+      //   child: Container(
+      //     width: double.infinity,
+      //     height: MediaQuery.of(context).size.height,
+      //     color: Colors.grey,
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         Padding(
+      //           padding: const EdgeInsets.all(15.0),
+      //           child: Container(
+      //             decoration: BoxDecoration(
+      //               borderRadius: BorderRadius.circular(15),
+      //               color: Colors.white,
+      //             ),
+      //             width: double.infinity,
+      //             height: 350,
+      //
+      //
+      //             child: Column(
+      //               children: [
+      //                 Padding(
+      //                   padding: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+      //                   child: Container(
+      //                     decoration: BoxDecoration(
+      //                       border: Border.all(color: Colors.teal, width: 2),
+      //                       borderRadius: BorderRadius.circular(15),
+      //                     ),
+      //                     child: TextFormField(
+      //
+      //                       autofocus: false,
+      //                       controller: nameEditingController,
+      //                       keyboardType: TextInputType.name,
+      //                       validator: (value) {
+      //                         RegExp regex = new RegExp(r'^.{3,}$');
+      //                         if (value!.isEmpty) {
+      //                           return ("Name cannot be Empty");
+      //                         }
+      //
+      //                         if (!regex.hasMatch(value)) {
+      //                           return ("Enter Valid Name(Min. 3 character)");
+      //                         }
+      //                         return null;
+      //                       },
+      //                       onSaved: (value){
+      //                         nameEditingController.text = value!;
+      //                       },
+      //                       textInputAction: TextInputAction.next,
+      //                       decoration: InputDecoration(
+      //                         border: InputBorder.none,
+      //
+      //
+      //                         contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+      //                         hintText: "Task Name",
+      //
+      //                       ),
+      //                     ),
+      //                   ),
+      //                 ),
+      //
+      //                 Padding(
+      //                   padding: EdgeInsets.all(10.0),
+      //                   child: Container(
+      //                     padding: EdgeInsets.only(left: 10, right: 10),
+      //                     decoration: BoxDecoration(
+      //                       border: Border.all(color: Colors.teal, width: 2),
+      //                       borderRadius: BorderRadius.circular(15),
+      //                     ),
+      //                     child: DropdownButton(
+      //                       isExpanded: true,
+      //                       icon: Icon(Icons.arrow_drop_down),
+      //                       underline: SizedBox(),
+      //                       iconSize: 36,
+      //                       hint: Text("Select Department"),
+      //                       value: valueChoose1,
+      //                       onChanged: (newValue) {
+      //                         setState(() {
+      //                           valueChoose1 = newValue as String?;
+      //                         });
+      //                       },
+      //                       items: item1.map((valueItem) {
+      //                         return DropdownMenuItem(
+      //                           value: valueItem,
+      //                           child: Text(valueItem),
+      //                         );
+      //                       }).toList(),
+      //                     ),
+      //                   ),
+      //                 ),
+      //
+      //                 Text(
+      //                   "Dead Line",
+      //                   style:
+      //                       TextStyle(
+      //                     fontWeight: FontWeight.bold,
+      //                         fontSize: 18,
+      //                         color: Colors.red,
+      //                   ),
+      //                   ),
+      //
+      //                 SizedBox(
+      //                   height: 10,
+      //                 ),
+      //
+      //
+      //
+      //
+      //                 Padding(
+      //                   padding: const EdgeInsets.only(left: 15, right: 15),
+      //                   child: Row(
+      //                     mainAxisAlignment: MainAxisAlignment.center,
+      //                     children: [
+      //                       SizedBox(
+      //                         width: 130,
+      //                         height: 45,
+      //                         child: Expanded(
+      //                           child: ElevatedButton(
+      //                             child: Text(buttonText),
+      //                             onPressed: () {
+      //                               setState(() {
+      //                                 buttonText = "${start.day}/${start.month}/${start.year}";
+      //                               });
+      //
+      //                             },
+      //                             style: ButtonStyle(
+      //                               backgroundColor: MaterialStateProperty.all(Colors.teal),
+      //                             ),
+      //
+      //                           ),
+      //                         ),
+      //                       ),
+      //
+      //                       SizedBox(width: 14),
+      //
+      //                       SizedBox(
+      //                         height: 45,
+      //                         width: 130,
+      //                         child: Expanded(
+      //                           child: ElevatedButton(
+      //                             child: Text(buttonText1),
+      //                             onPressed: () {
+      //                               setState(() {
+      //                                 buttonText1 = "${end.day}/${end.month}/${end.year}";
+      //                               });
+      //                             },
+      //                             style: ButtonStyle(
+      //                               backgroundColor: MaterialStateProperty.all(Colors.teal),
+      //                             ),
+      //                           ),
+      //                         ),
+      //                       ),
+      //
+      //
+      //                     ],
+      //                   ),
+      //                 ),
+      //
+      //
+      //               ],
+      //             ),
+      //
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+    );
   }
+
 }
